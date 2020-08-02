bd275701739acfd7728b2dd76a9e972da4392a3b
Pradyut Tiwari <43323732+pradyutdec@users.noreply.github.com>
2020-08-02 19:53:25 +0100

    Add files via upload

---
+++ ML-Twitter.py
@@ -0,0 +1,168 @@
+import tweepy
+from tweepy import OAuthHandler
+ 
+consumer_key = 'YOUR-CONSUMER-KEY'
+consumer_secret = 'YOUR-CONSUMER-SECRET'
+access_token = 'YOUR-ACCESS-TOKEN'
+access_secret = 'YOUR-ACCESS-SECRET'
+ 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    print(status.text)
+	
+
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+	
+
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+ 
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
+
+import json
+ 
+with open('mytweets.json', 'r') as f:
+    line = f.readline() # read only the first tweet/line
+    tweet = json.loads(line) # load it as Python dict
+    print(json.dumps(tweet, indent=4)) # pretty-print
+	
+from nltk.tokenize import word_tokenize
+ 
+tweet = 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
+print(word_tokenize(tweet))
+# ['RT', '@', 'marcobonzanini', ':', 'just', 'an', 'example', '!', ':', 'D', 'http', ':', '//example.com', '#', 'NLP']
+
+import re
+ 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+ 
+tweet = 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
+print(preprocess(tweet))
+# ['RT', '@marcobonzanini', ':', 'just', 'an', 'example', '!', ':D', 'http://example.com', '#NLP']
+
+with open('mytweets.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line)
+        tokens = preprocess(tweet['text'])
+        do_something_else(tokens)
+		
+import vincent
+ 
+word_freq = count_terms_only.most_common(20)
+labels, freq = zip(*word_freq)
+data = {'data': freq, 'x': labels}
+bar = vincent.Bar(data, iter_idx='x')
+bar.to_json('term_freq.json')
+
+<html>  
+<head>    
+    <title>Vega Scaffold</title>
+    <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    <script src="http://d3js.org/topojson.v1.min.js"></script>
+    <script src="http://d3js.org/d3.geo.projection.v0.min.js" charset="utf-8"></script>
+    <script src="http://trifacta.github.com/vega/vega.js"></script>
+</head>
+<body>
+    <div id="vis"></div>
+</body>
+<script type="text/javascript">
+// parse a spec and create a visualization view
+function parse(spec) {
+  vg.parse.spec(spec, function(chart) { chart({el:"#vis"}).update(); });
+}
+parse("term_freq.json");
+</script>
+</html>
+
+import pandas
+import json
+ 
+dates_ITAvWAL = []
+# f is the file pointer to the JSON data set
+for line in f:
+    tweet = json.loads(line)
+    # let's focus on hashtags only at the moment
+    terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+    # track when the hashtag is mentioned
+    if '#itavwal' in terms_hash:
+        dates_ITAvWAL.append(tweet['created_at'])
+ 
+# a list of "1" to count the hashtags
+ones = [1]*len(dates_ITAvWAL)
+# the index of the series
+idx = pandas.DatetimeIndex(dates_ITAvWAL)
+# the actual series (at series of 1s for the moment)
+ITAvWAL = pandas.Series(ones, index=idx)
+ 
+# Resampling / bucketing
+per_minute = ITAvWAL.resample('1Min', how='sum').fillna(0)
+
+time_chart = vincent.Line(ITAvWAL)
+time_chart.axis_titles(x='Time', y='Freq')
+time_chart.to_json('time_chart.json')
+
+# all the data together
+match_data = dict(ITAvWAL=per_minute_i, SCOvIRE=per_minute_s, ENGvFRA=per_minute_e)
+# we need a DataFrame, to accommodate multiple series
+all_matches = pandas.DataFrame(data=match_data,
+                               index=per_minute_i.index)
+# Resampling as above
+all_matches = all_matches.resample('1Min', how='sum').fillna(0)
+ 
+# and now the plotting
+time_chart = vincent.Line(all_matches[['ITAvWAL', 'SCOvIRE', 'ENGvFRA']])
+time_chart.axis_titles(x='Time', y='Freq')
+time_chart.legend(title='Matches')
+time_chart.to_json('time_chart.json')
